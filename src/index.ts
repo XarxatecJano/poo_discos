aaae60879c2ef28235430a57e423ead584034980
Jano Soler <janosolerdiaz@gmail.com>
2024-09-23 11:36:01 +0200

    más cambios

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,5 @@ app.get('/', (req: Express.Request, res: Express.Response) => {
 });
 
 app.listen(port, () => {
-<<<<<<< HEAD
-  console.log(`App estupenda de Jano escuchando en puerto ${port}`);
-=======
   console.log(`App de Alejandro escuchando en puerto ${port}`);
->>>>>>> cf7361b09e910407891e4a9a4199d64e919f4e25
 });
